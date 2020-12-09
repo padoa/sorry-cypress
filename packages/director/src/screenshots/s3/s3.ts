7a914af54b12adec5c309ce35467e6944f7159e9
Paul Lesur <paul.lesur@free.fr>
2020-12-09 15:02:46 +0100

    New version

--- packages/director/src/screenshots/s3/s3.ts
+++ packages/director/src/screenshots/s3/s3.ts
@@ -1,25 +1,23 @@
 import aws from 'aws-sdk';
 import {
+  S3_BUCKET,
   S3_REGION,
+  S3_ACL,
+  S3_READ_URL_PREFIX,
   S3_IMAGE_KEY_PREFIX,
   S3_VIDEO_KEY_PREFIX,
-  S3_BUCKET,
-  FILES_EXPIRATION,
-  S3_ENDPOINT,
-  S3_FORCE_PATH_STYLE,
 } from './config';
 import { S3SignedUploadResult } from './types';
 import { AssetUploadInstruction } from '@src/types';
 import { sanitizeS3KeyPrefix } from './utils';
 
+const BUCKET_URL = `https://${S3_BUCKET}.s3.amazonaws.com`;
 const ImageContentType = 'image/png';
 const VideoContentType = 'video/mp4';
 
 const s3 = new aws.S3({
   region: S3_REGION,
   signatureVersion: 'v4',
-  endpoint: S3_ENDPOINT,
-  s3ForcePathStyle: S3_FORCE_PATH_STYLE,
 });
 
 interface GetUploadURLParams {
@@ -31,31 +29,26 @@ interface GetUploadURLParams {
 export const getUploadUrl = async ({
   key,
   ContentType = ImageContentType,
-  Expires = FILES_EXPIRATION,
+  Expires = 60,
 }: GetUploadURLParams): Promise<S3SignedUploadResult> => {
-  const s3ParamsRead = {
+  const s3Params = {
     Bucket: S3_BUCKET,
-    Expires,
     Key: key,
-  };
-
-  const s3ParamsUpload = {
     Expires,
     ContentType,
-    ...s3ParamsRead,
+    ACL: S3_ACL,
   };
 
   return new Promise((resolve, reject) => {
-    const signedReadURL = s3.getSignedUrl('getObject', s3ParamsRead);
     s3.getSignedUrl(
       'putObject',
-      s3ParamsUpload,
+      s3Params,
       (error: Error, uploadUrl: string) => {
         if (error) {
           return reject(error);
         }
         return resolve({
-          readUrl: signedReadURL,
+          readUrl: `${S3_READ_URL_PREFIX || BUCKET_URL}/${key}`,
           uploadUrl,
         });
       }
@@ -68,7 +61,6 @@ export const getImageUploadUrl = async (
 ): Promise<AssetUploadInstruction> =>
   getUploadUrl({
     key: `${sanitizeS3KeyPrefix(S3_IMAGE_KEY_PREFIX)}${key}.png`,
-    Expires: FILES_EXPIRATION,
   });
 
 export const getVideoUploadUrl = async (
@@ -77,5 +69,5 @@ export const getVideoUploadUrl = async (
   getUploadUrl({
     key: `${sanitizeS3KeyPrefix(S3_VIDEO_KEY_PREFIX)}${key}.mp4`,
     ContentType: VideoContentType,
-    Expires: FILES_EXPIRATION,
+    Expires: 90,
   });
