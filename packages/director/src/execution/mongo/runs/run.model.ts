1e42fcb502e4cb5578e9a6737a9e782273624c30
Hackatosh <edouard.benauw@student.ecp.fr>
2021-05-20 16:53:13 +0200

    [TECH](SRE) Fix update errors

--- packages/director/src/execution/mongo/runs/run.model.ts
+++ packages/director/src/execution/mongo/runs/run.model.ts
@@ -100,7 +100,7 @@ export const setSpecClaimed = async (
   instanceId: string,
   machineId: string
 ) => {
-  const { matchedCount, modifiedCount } = await getMongoDB()
+  const { result } = await getMongoDB()
     .collection('runs')
     .updateOne(
       {
@@ -124,9 +124,9 @@ export const setSpecClaimed = async (
       }
     );
 
-  if (matchedCount && modifiedCount) {
+  if (result.ok === 1) {
     return;
   } else {
-    throw new AppError(CLAIM_FAILED, `Claim failed for instance id ${instanceId}, run id ${runId} and machine id ${machineId}. ${ matchedCount ? 'No element modified' : 'No element found'}`);
+    throw new AppError(CLAIM_FAILED, `Claim failed for instance id ${instanceId}, run id ${runId} and machine id ${machineId}.`);
   }
 };
