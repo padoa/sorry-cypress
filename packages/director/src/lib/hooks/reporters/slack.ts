ec9eee6921ccd9d353653b96c9e63ec7a3c5a865
Ian Ducrot--Thollon-Pommerol <ian.d@padoa-group.com>
2022-10-25 17:42:17 +0200

    More logging on slack hook error

--- packages/director/src/lib/hooks/reporters/slack.ts
+++ packages/director/src/lib/hooks/reporters/slack.ts
@@ -146,7 +146,31 @@ export async function reportToSlack(
       ],
       icon_url: 'https://sorry-cypress.s3.amazonaws.com/images/logo-bg.png',
     },
-  }).catch((error) => {
+  }).then(function (response) {
+    console.log("[CUSTOM] SLACK HOOK SUCESS");
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  })
+  .catch((error) => {
+    console.log("[CUSTOM] SLACK HOOK ERROR");
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
     getLogger().error(
       { error, ...hook },
       `Error while posting Slack message to ${hook.url}`
